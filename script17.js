465f94cd8b5ffa0daa69c70ba0aa3561b0420c25
luiz769 <luizbueno468kbg@gmail.com>
2024-11-05 20:09:49 -0300

    Add files via upload
    Somente sons aleatórios

---
+++ script17.js
@@ -0,0 +1,21 @@
+function tocarMusica() {
+    const audio = document.getElementById('minhaMusica');
+    audio.currentTime = 0; // Reinicia o áudio
+    audio.play();
+}
+
+function tocarMusica2() {
+    const audio = document.getElementById('minhaMusica2');
+    audio.currentTime = 0; // Reinicia o áudio
+    audio.play();
+}
+
+function tocarMusica3() {
+    const audio = document.getElementById('minhaMusica3');
+    audio.currentTime = 0; // Reinicia o áudio
+    audio.play();
+}
+
+function abrirLink() {
+    window.open("https://youtu.be/a63jpBsVZYw?si=ucOkfK2AoHcNwBhl&t=11", "_blank");
+}
